3763073d0e6d966c67de63a8065329a62cf3a4fa
oragamiman01 <44078735+oragamiman01@users.noreply.github.com>
2024-07-09 18:54:32 -0600

    finally got collapsibles working

--- js/main.js
+++ js/main.js
@@ -102,16 +102,32 @@
 	}
 
 	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
+	// $('#testimonial-mf').owlCarousel({
+	// 	margin: 20,
+	// 	autoplay: true,
+	// 	autoplayTimeout: 4000,
+	// 	autoplayHoverPause: true,
+	// 	responsive: {
+	// 		0: {
+	// 			items: 1,
+	// 		}
+	// 	}
+	// });
+
+	
+    var coll = document.getElementsByClassName("collapsible");
+	var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.parentNode.nextElementSibling.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
 
 })(jQuery);
